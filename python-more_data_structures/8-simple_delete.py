3b1543ecdab917d9581fe16f8bc2602d4d18c365
aishalieva0 <aysalieva6@gmal.com>
2024-02-04 09:10:04 -0800

    fix error

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary is None:
-        print("Error: Dictionary is None.")
-        return
-
-    if not isinstance(a_dictionary, dict):
-        print("Error: Input is not a dictionary.")
-        return
-
     if key in a_dictionary:
         del a_dictionary[key]
+
+    return a_dictionary
