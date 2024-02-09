1c731e4c7eef3d25514eede1d777dd3c165fdc88
aishalieva0 <aysalieva6@gmal.com>
2024-02-09 11:04:42 -0800

    doc

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,6 +4,18 @@ Contains class Rectangle
 with private attribute width and height
 """
 class Rectangle:
+    """
+    Defines a class rectangle with private attribute width and height
+    Args:
+        width (int): width
+        height (int): height
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
