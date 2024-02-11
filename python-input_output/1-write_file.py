0bffe7e6d291b8489ca73d96941af6216724f70c
aishalieva0 <aysalieva6@gmal.com>
2024-02-11 11:54:27 -0800

    tasks

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,6 @@ Contains function that writes to text file and returns num chars written
 
 
 def write_file(filename="", text=""):
-    """writes to text file and retunrs num chars written"""
+    """Writes to text file and returns num chars written"""
     with open(filename, 'w', encoding="utf-8") as f:
-        return(f.write(text))
+        return f.write(text)
--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -8,8 +8,8 @@ import json
 
 
 def from_json_string(my_str):
-
-
-    """function that returns an object (Python data strcuture)
-    represented by a JSON string"""
-    return(json.loads(my_str))
+    """
+    Function that returns an object (Python data structure)
+    represented by a JSON string
+    """
+    return json.loads(my_str)
