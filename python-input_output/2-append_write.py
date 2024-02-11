6cb7a70fa8fd16ad778534489ef202b34725f73e
aishalieva0 <aysalieva6@gmal.com>
2024-02-11 11:51:47 -0800

    tasks

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """appends to text file and returns num chars added"""
+    """Appends to text file and returns num chars added"""
     with open(filename, 'a', encoding="utf-8") as f:
-        return(f.write(text))
+        return f.write(text)
