986080a2c42fb703e8603ea80490583b21489270
aishalieva0 <aysalieva6@gmal.com>
2024-02-11 11:48:46 -0800

    tasks

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -2,10 +2,14 @@
 """
 Contains function that returns python data structure from JSON string
 """
+
+
 import json
 
 
 def from_json_string(my_str):
+
+
     """function that returns an object (Python data strcuture)
     represented by a JSON string"""
     return(json.loads(my_str))
