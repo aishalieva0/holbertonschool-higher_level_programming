2cceddac602df76c5993a097a9caa665db5ad6e5
aishalieva0 <aysalieva6@gmal.com>
2024-02-16 07:52:16 -0800

    task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+model that contains class Base
+"""
+
+
+class Base:
+    """
+    defines class Base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
