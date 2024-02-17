9de46e5a27dbcd2833a1179c3b3bc4fde4173ad3
aishalieva0 <aysalieva6@gmal.com>
2024-02-17 08:12:39 -0800

    added test case

---
+++ python-almost_a_circle/tests/test_modules/test_base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+test for Base class
+"""
+import unittest
+from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
+
+
+
+class TestBase(unittest.Testcase):
+    """
+    class to test class Base
+    """
+    def test_auto_assign_id(self):
+        base1 = Base()
+        base2 = Base()
+        base3 = Base()
+
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
+        self.assertEqual(base3.id, 3)
