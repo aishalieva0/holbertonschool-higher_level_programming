79cb5322cdc374cf83ef0a7da82681afb602564e
aishalieva0 <aysalieva6@gmal.com>
2024-02-17 02:34:56 -0800

    task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+inherits class Base
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.width)
+        return "[Square]" + str_id + str_xy + str_size
+
