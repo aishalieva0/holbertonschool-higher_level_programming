337985e23b702a8583a07173b1c0b1bf3c698538
aishalieva0 <aysalieva6@gmal.com>
2024-02-17 10:32:02 -0800

    rectangle teszts

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,17 +13,44 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rect.y, 0)
         self.assertIsNotNone(rect.id)
 
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+        with self.assertRaises(TypeError):
+            Rectangle(1, "2")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, -2)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, -3)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, 3, -4)
+
     def test_area(self):
         rect = Rectangle(5, 10)
         self.assertEqual(rect.area(), 50)
 
-    def test_display(self):
-        rect = Rectangle(3, 2)
-        self.assertEqual(rect.display(), None)  # Assuming display just prints, returns None
-
     def test_str(self):
         rect = Rectangle(4, 8, 2, 3, 1)
         self.assertEqual(str(rect), "[Rectangle] (1) 2/3 - 4/8")
+
+    def test_display(self):
+        rect = Rectangle(3, 2)
+        self.assertEqual(rect.display(), None)
+
+    def test_to_dictionary(self):
+        rect = Rectangle(10, 20, 5, 5, 1)
+        rect_dict = rect.to_dictionary()
+        self.assertEqual(rect_dict, {'id': 1, 'width': 10, 'height': 20, 'x': 5, 'y': 5})
 
     def test_update(self):
         rect = Rectangle(1, 1, 0, 0, 1)
@@ -34,7 +61,37 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rect.x, 5)
         self.assertEqual(rect.y, 6)
 
-    def test_to_dictionary(self):
-        rect = Rectangle(10, 20, 5, 5, 1)
-        rect_dict = rect.to_dictionary()
-        self.assertEqual(rect_dict, {'id': 1, 'width': 10, 'height': 20, 'x': 5, 'y': 5})
+        rect.update(89)
+        self.assertEqual(rect.id, 89)
+        rect.update(89, 1)
+        self.assertEqual(rect.width, 1)
+        rect.update(89, 1, 2)
+        self.assertEqual(rect.height, 2)
+        rect.update(89, 1, 2, 3)
+        self.assertEqual(rect.x, 3)
+        rect.update(89, 1, 2, 3, 4)
+        self.assertEqual(rect.y, 4)
+
+        rect.update(**{'id': 89})
+        self.assertEqual(rect.id, 89)
+        rect.update(**{'id': 89, 'width': 1})
+        self.assertEqual(rect.width, 1)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual(rect.height, 2)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual(rect.x, 3)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual(rect.y, 4)
+
+    def test_create(self):
+        rect = Rectangle.create(**{'id': 89})
+        self.assertEqual(rect.id, 89)
+        rect = Rectangle.create(**{'id': 89, 'width': 1})
+        self.assertEqual(rect.width, 1)
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual(rect.height, 2)
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual(rect.x, 3)
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual(rect.y, 4)
+
