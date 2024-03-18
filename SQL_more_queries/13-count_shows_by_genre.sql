cfd998fd8c490b519aa5dd4dd44da966c5198c23
aishalieva0 <aysalieva6@gmail.com>
2024-03-18 11:39:06 +0000

    correction

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- ists all genres from hbtn_0d_tvshows and displays the number of shows
+-- ists all genres from hbtn_0d_tvshows and displays number of shows
 SELECT g.name genre, COUNT(g.id) number_of_shows FROM tv_genres g
 	LEFT JOIN tv_show_genres tg ON g.id = tg.genre_id
 	GROUP BY (g.id)
