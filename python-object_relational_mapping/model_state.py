b464fff45eceb8de1f3e725b2e8a611435fc4416
aishalieva0 <aysalieva6@gmail.com>
2024-03-22 09:02:41 -0700

    task 14

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,6 +4,7 @@ contains the class definition of a State
 an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -14,3 +15,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    cities = relationship('City', back_populates='state')
