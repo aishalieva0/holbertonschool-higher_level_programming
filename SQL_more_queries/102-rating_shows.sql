a9254acf6524a4b8b159a8ceda66c16660eabf65
aishalieva0 <aysalieva6@gmail.com>
2024-03-22 13:43:51 -0700

    task 19

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- lists all shows by their rating
-SELECT s.title, SUM(r.rate) rating from tv_shows s 
-	JOIN tv_show_ratings r on s.id=r.show_id
+SELECT s.title, SUM(r.rate) rating FROM tv_shows s 
+	JOIN tv_show_ratings r ON s.id=r.show_id
 	GROUP BY s.title
 	ORDER BY rating DESC;
