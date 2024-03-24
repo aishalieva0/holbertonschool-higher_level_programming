2240568a1fee2dd150ac7176686c61f25bb761ae
aishalieva0 <aysalieva6@gmail.com>
2024-03-24 09:49:09 -0700

    task 15

---
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" creates the State California with the City San Francisco """
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def main():
+    usr = sys.argv[1]
+    pswd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(usr, pswd, db))
+
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='California')
+    new_city = City(name='San Francisco', state=new_state)
+
+    session.add(new_state)
+    session.add(new_city)
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,11 @@
+""" contains the class definition of a City """
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """ model City """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
